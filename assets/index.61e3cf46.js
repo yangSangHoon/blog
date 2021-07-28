b57cab162494e3df394005bdbbd6518265c8d7a7
sanghoon <dazzybana@gmail.com>
2021-07-29 05:26:02 +0900

    Updates

---
+++ assets/index.61e3cf46.js
@@ -0,0 +1,15 @@
+import{q as e,R as t,H as l,L as n,a,b as r}from"./vendor.206d61b2.js";const m=e.div`
+  padding: 20px;
+  h2 {
+    margin-top: 50px;
+    display: block;
+    font-size: 30px;
+  }
+
+  div {
+    position: relative;
+    z-index: 1;
+    background-size: 300px auto;
+    background-repeat: no-repeat;
+  }
+`;function c(){return t.createElement(m,null,t.createElement("h2",null,"2021.7.25"),t.createElement("img",{src:"/blog/images/20210725.png",width:"100"}),t.createElement("div",{className:"d20210725"},t.createElement("p",null,"Fell A Coffe를 갔는데 초코렛 들어간 우유도 맛있고 과자 서비스도 많이 주시고 30분 마시고 나오는데 서비스로 아메리카노를 무료로 주셨다. 오픈한지 얼마 안되서 친절한 것 같은데 사장님이 전략적으로 하시는 것인지 모르겠지만 오랜만에 마음에 드는 친절한 커피숍을 발견한 것 같아 기분이 좋았다.")),t.createElement("h2",null,"2021.7.27"),t.createElement("img",{src:"/blog/images/20210727.png",width:"200"}),t.createElement("div",{className:"d20210727"},t.createElement("p",null,"행복이랑 놀았다. i had a time that i play with 행복이 ")),t.createElement("h2",null,"2021.7.28"),t.createElement("img",{src:"/blog/images/20210728.png",width:"200"}),t.createElement("div",null,t.createElement("p",null,"New hires of two guys entered my company",t.createElement("br",null),"I many talked this weekly meeting first time",t.createElement("br",null),"Subject of talk is Code-Review about my jog(gigs project)",t.createElement("br",null)," 두명의 신규 입사자가 회사에 입사했다",t.createElement("br",null),"처음으로 주간 회의떄 말을 많이 했다",t.createElement("br",null),"대화의 주제는 내 일(긱스 프로젝트)에 대한 코드 리뷰였다")))}function i(){return t.createElement(l,{basename:"/blog"},t.createElement("h1",null,"양상훈의 블로그"),t.createElement(n,{to:"/daily"},"일기"),t.createElement(a,{path:"/daily",exact:!0,component:c}))}r.render(t.createElement(t.StrictMode,null,t.createElement(i,null)),document.getElementById("root"));
