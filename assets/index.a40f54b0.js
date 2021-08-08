573f31ca1e523a2bfe4a53005dca74fa0381da4f
sanghoon <dazzybana@gmail.com>
2021-08-08 23:18:26 +0900

    Updates

---
+++ assets/index.a40f54b0.js
@@ -0,0 +1,37 @@
+import{q as e,R as t,H as l,L as n,a,b as r}from"./vendor.206d61b2.js";const c=e.div`
+  h2 {
+    margin-top: 50px;
+    display: block;
+    font-size: 30px;
+  }
+
+  div {
+    position: relative;
+    z-index: 1;
+    background-size: 300px auto;
+    background-repeat: no-repeat;
+  }
+`;function m({date:e,text:l,img:n,imgWidth:a}){return t.createElement(c,null,t.createElement("h2",null,e),n&&t.createElement("img",{src:n,width:null!=a?a:300}),t.createElement("div",null,l))}const i=e.div`
+  padding: 20px;
+`;function o(){return t.createElement(i,null,t.createElement(m,{date:"2021.7.25",img:"/blog/images/20210725.png",imgWidth:"100",text:"Fell A Coffe를 갔는데 초코렛 들어간 우유도 맛있고 과자 서비스도 많이 주시고 30분 마시고 나오는데 서비스로 아메리카노를 무료로 주셨다.\n          오픈한지 얼마 안되서 친절한 것 같은데 사장님이 전략적으로 하시는 것인지 모르겠지만 오랜만에 마음에 드는 친절한 커피숍을 발견한 것 같아\n          기분이 좋았다."}),t.createElement(m,{date:"2021.7.27",img:"/blog/images/20210727.png",text:"행복이랑 놀았다. i had a time that i play with 행복이 "}),t.createElement(m,{date:"2021.7.28",img:"/blog/images/20210728.png",text:t.createElement("p",null,"The Tow of guys entered my company.",t.createElement("br",null),"it is first time that I talk much in a weekly meeting.",t.createElement("br",null),"The subject of talk is Code-Review about my jog(gigs project).",t.createElement("br",null)," 두명의 신규 입사자가 회사에 입사했다.",t.createElement("br",null),"처음으로 주간 회의떄 말을 많이 했다.",t.createElement("br",null),"대화의 주제는 내 일(긱스 프로젝트)에 대한 코드 리뷰였다.")}),t.createElement(m,{date:"2021.7.29",img:"/blog/images/20210729.png",imgWidth:"400",text:t.createElement("p",null,"I bougth a 금강제화 gift card for 세정's father birthday.",t.createElement("br",null),"And We bought a coffee and a cake from the starbuck.",t.createElement("br",null),"And We went to chaire in lotte tower front square.",t.createElement("br",null),"So We ate they.",t.createElement("br",null),"By the away The woman on a bicycle told us.",t.createElement("br",null),"She say that believe hallelujah.")}),t.createElement(m,{date:"2021.7.30",img:"/blog/images/20210730.png",imgWidth:"400",text:t.createElement("p",null,"Today is 세정's father birthday.",t.createElement("br",null),"We went to the 세정's home after finish my work.",t.createElement("br",null),"there are many foods. Sliced raw fish of Salmon and Flatfish , Sea cucumber, octopus, small octopus, spicy soup.",t.createElement("br",null),"Spicy soup was the most impressive in today.",t.createElement("br",null),"It's very delicious.")}),t.createElement(m,{date:"2021.8.01",img:"/blog/images/20210801.png",imgWidth:"400",text:t.createElement("p",null,"i want to be freeman.",t.createElement("br",null),"i many watched the youtube videos.",t.createElement("br",null),"i watched a youtube about the 안성시's a house and a land investment.")}),t.createElement(m,{date:"2021.8.04",img:"/blog/images/20210804.png",imgWidth:"400",text:t.createElement("p",null,"There were meeting to 세정's english study in the 역삼.",t.createElement("br",null)," I went the 역삼 too.",t.createElement("br",null)," And i ride a bicycle during 세정's meeting.",t.createElement("br",null)," i took a big circle around 강남.")}),t.createElement(m,{date:"2021.8.05",text:t.createElement(t.Fragment,null,"최근에 프리즌 브레이크를 다시 보고 있다.",t.createElement("br",null),"역시 캐릭터들이 개성있어야 재미있는것 같다.",t.createElement("br",null),"아침에는 올림픽 공원가는길에 뻐칼국수를 먹었다. ",t.createElement("br",null),"맛있었다.",t.createElement("br",null),"Recent, I am reWatching the PrisonBreak.",t.createElement("br",null),"As expected, I think Darama funny point with charactor's personalaty.",t.createElement("br",null),"i had Bone chopped noodles during way to olympic park.",t.createElement("br",null),"it is delicious.")}))}const u=e.div`
+  h2 {
+    margin-top: 50px;
+    display: block;
+    font-size: 30px;
+  }
+
+  div {
+    position: relative;
+    z-index: 1;
+    background-size: 300px auto;
+    background-repeat: no-repeat;
+  }
+`;function E({title:e,content:l}){return t.createElement(u,null,t.createElement("h2",null,e),t.createElement("div",null,l))}const d=e.div`
+  padding: 20px;
+`;function s(){return t.createElement(d,null,t.createElement(E,{title:"사랑은 풍선 같은 것",content:t.createElement(t.Fragment,null,"사랑은 풍선 같은것",t.createElement("br",null),"너무 커지면 뻥 하고 터진다",t.createElement("br",null),"너무 힘을 빼면 쓩 하고 작아진다",t.createElement("br",null),"적절하게 부풀어 올랐을때 못 달아나게 묶어 보았다",t.createElement("br",null),"이제됐다 싶으면 두둥실 날아가 버렸다",t.createElement("br",null),"알록 달록 고운 풍선 언젠가는 남으리",t.createElement("br",null),"평생 함께할 풍선")}),t.createElement(E,{title:"마시멜로와 입술의 상관 관계",content:t.createElement(t.Fragment,null,"말랑말랑 마시멜로",t.createElement("br",null),"말랑말랑 너의 입술",t.createElement("br",null),"오물오물 맛있다",t.createElement("br",null),"달짝지근 맛있다",t.createElement("br",null),"내 입술안에 녹아내리는",t.createElement("br",null),"마시멜로와 너의 입술")}))}const b=e.div`
+  padding: 20px;
+`;function g(){return t.createElement(b,null,t.createElement(E,{title:"팬데믹 머니",content:t.createElement(t.Fragment,null,"팬데믹 머니라는 다큐를 보고 정리(2021.5.28 방영)",t.createElement("ul",null,t.createElement("li",null,"기업은 저 금리로 돈을 빌려 자사 주를 매입하여 배당금으로 3~4프로의 수익을 올렸다"),t.createElement("li",null,"세계 상위 1%기업 애플, 마이크로소프트, 테슬라 등이 그러했으며 삼성도 이번에 배당금이 많은 이유가 본인들의 수익을 높이기 위함일 것 같다"),t.createElement("li",null,"금융위기를 막으려고 4조이상의 양적완화를 2008년에 시행하고 아직 정상화 하기 전인데 펜데믹 이후에 5조 + 6조 계속 양적왕화를 하고 있다"),t.createElement("li",null,"자산의 금액을 높이면 사람들이 자신이 돈이 많아 진 것으로 착각을 하여 돈을 더 많이 쓰게 만들어 내수시장을 높이기 위한 전략이다"),t.createElement("li",null,"2024년 까지 저금리 유지한다고 연준이 밝힌 상황, 집을 사서 주택대출을 이용하고 신용대출을 받아 투자를 하는 것이 수익률이 높은 시기"),t.createElement("li",null,"2023년 대통령 선거 지나고, 24년쯤 되면 버블이 터질 것 같다"),t.createElement("li",null,"기업의 가치를 보고 투자 하지 않고 오를만한 주식을 투자한다. 밈 주식 시대가 왔다.(도지 코인 같이)")))}))}const p=e.div`
+  display: flex;
+  gap: 20px;
+  align-items: center;
+`;function h(){return t.createElement(l,{basename:"/blog"},t.createElement(p,null,t.createElement("h1",null,"양상훈의 블로그"),t.createElement(n,{to:"/daily"},"일기"),t.createElement(n,{to:"/writing"},"시"),t.createElement(n,{to:"/economy"},"경제")),t.createElement(a,{path:"/daily",exact:!0,component:o}),t.createElement(a,{path:"/writing",exact:!0,component:s}),t.createElement(a,{path:"/economy",exact:!0,component:g}))}r.render(t.createElement(t.StrictMode,null,t.createElement(h,null)),document.getElementById("root"));
